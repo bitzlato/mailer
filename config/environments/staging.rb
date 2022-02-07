28dc35a55fb6b78bd337a520e53797415b2f41c1
Artur Egorov <artur.e@bitzlato.com>
2022-02-07 16:00:00 +0300

    [#8] json logs format for staging and production

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,5 @@
 require "active_support/core_ext/integer/time"
+require Rails.root.join('lib', 'mailer', 'json_log_formatter')
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -59,4 +60,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.log_formatter = JsonLogFormatter.new
 end
