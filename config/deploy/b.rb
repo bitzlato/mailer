a8e2d0ffd73dab3f7ed714254f8b2d0a6b0c675b
Dmitriy ALekseev <veeskelaam@gmail.com>
2022-05-26 10:42:37 +0300

    b-www deploy

---
+++ config/deploy/b.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+set :user, 'app'
+
+set :rails_env, :staging
+set :disallow_pushing, false
+set :application, -> { 'mailer-' + fetch(:stage).to_s }
+
+set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:stage)}/#{fetch(:application)}" }
+
+server ENV.fetch('STAGING_SERVER'),
+  user: fetch(:user),
+  port: '22',
+  roles: %w[app mailer db].freeze,
+  ssh_options: { forward_agent: true }
