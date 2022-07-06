6677b13814584d99b65eec27cd08b353b6d7926b
Константин Алексеев <k.alekseev@semrush.com>
2022-07-06 22:40:05 +0300

    deploy for arnika

---
+++ config/deploy/arnika.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+set :stage, :production
+set :rails_env, :production
+fetch(:default_env)[:rails_env] = :production
+
+server ENV.fetch('PRODUCTION_SERVER_ARNIKA'),
+       user: 'app',
+       port: '22',
+       roles: %w[app mailer].freeze,
+       ssh_options: { forward_agent: true }
